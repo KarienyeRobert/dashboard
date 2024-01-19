8d5198182f4f301c3dfa014f1ea4d6b36b87a4a7
KarienyeRobert <karienyerobert@gmail.com>
2024-01-19 13:06:30 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { fiSettings } from 'react-icons/fi'
+import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 
 import './app.css'
 
 const App = () => {
   return (
-    <h1 className='underline text-3xl'>App</h1>
+    <div>
+        <BrowserRouter>
+            <div className='flex relative dark:bg-main-dark-bg'></div>
+            <div className='fixed right-4 bottom-4' style={{zIndex: '1000'} }></div>
+        </BrowserRouter>
+    </div>
   )
 }
 
